073005797df6f1ef8da4bab9cfd545cb454a938b
Yaroslav <jaroslav.dashkevich@gmail.com>
2016-10-15 15:51:27 +0700

    Modify GET /echo to work with client in the same LAN

--- routes/echo.js
+++ routes/echo.js
@@ -12,7 +12,7 @@ router.get(
   {
     console.log(req.headers['x-real-ip']);
     if ( req.query.url && req.headers['x-real-ip'] && req.headers['x-real-ip'].match('192.168.1') ||
-        req.ip.match('127.0.0.1')
+        req.ip.match('127.0.0.1') || req.ip.match('192.168')
     )
     {
       proxy.makeRequest( req.query.url ).pipe( webRes );
