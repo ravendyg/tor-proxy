200e92b8508ec1439c1f15f6ecc4c3e5a2f1cc12
Yaroslav Dashkevich <y.dashkevich@amber.software>
2016-12-01 08:53:49 +0700

    Add time to error logging

--- routes/echo.js
+++ routes/echo.js
@@ -5,6 +5,7 @@ var express = require('express');
 var router = express.Router();
 var proxy = require('../lib/proxy');
 var errServ = require('../lib/error');
+const utils = require('../utils');
 
 router.get(
   '/',
@@ -31,14 +32,14 @@ router.get(
 
 function reqErrorHandler(res, err)
 {
-  console.error(err.message);
-  console.error(this.href);
+  console.error(utils.logDate(), err.message);
+  console.error(utils.logDate(), this.href);
   res.status(500).send();
 }
 
 function resErrorHandler(err)
 {
-  console.error(err);
+  console.error(utils.logDate(), err);
   this.status(500).send();
 }
 
