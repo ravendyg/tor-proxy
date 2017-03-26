efd7a9de9d3a979a3136eae75f30afeab5b6f664
Yaroslav <jaroslav.dashkevich@gmail.com>
2017-03-26 08:46:10 +0700

    Make it use several simultaneous connections if first call takes to much time

--- routes/echo.js
+++ routes/echo.js
@@ -3,35 +3,21 @@
 
 var express = require('express');
 var router = new express.Router();
+
 var proxy = require('../lib/proxy');
-const utils = require('../lib/utils');
 const config = require('../lib/config');
 
 router.get(
   '/',
   (req, res) => {
     if (config.AUTH_TOKEN && req.headers['x-auth-token'] === config.AUTH_TOKEN) {
-      let request = proxy.makeRequest(req.query.url);
-
-      request.on('error', reqErrorHandler.bind(request, res));
-      res.on('error', resErrorHandler);
-
-      request.pipe(res);
+      proxy.makeRequest(req.query.url, res);
     } else {
       res.status(400).send('bad request');
     }
   }
 );
 
-function reqErrorHandler(res, err) {
-  console.error(utils.logDate(), err.message);
-  console.error(utils.logDate(), this.href);
-  res.status(500).send();
-}
 
-function resErrorHandler(err) {
-  console.error(utils.logDate(), err);
-  this.status(500).send();
-}
 
 module.exports = router;
