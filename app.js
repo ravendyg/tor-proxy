6fe0528513fd73a826231a797326e7b5358ca654
Yaroslav <jaroslav.dashkevich@gmail.com>
2017-04-09 20:12:28 +0700

    Make each tor processes run on a separate thread, to get different ips

--- app.js
+++ app.js
@@ -4,23 +4,23 @@ var express = require('express');
 
 var bodyParser = require('body-parser');
 
+const test = require('./routes/test');
 const echo = require('./routes/echo');
 
 const config = require('./lib/config');
 
-var app = express();
 
+var app = express();
 
 app.use(bodyParser.json());
 
 
+app.use('/test', test);
 app.use('/echo', echo);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res) {
+  res.status(400).send();
 });
 
 // error handlers
@@ -42,17 +42,3 @@ app.use(
 
 
 module.exports = app;
-
-
-
-// services
-const proxy = require('./lib/proxy');
-const proxySettings = {
-  NUMBER_OF_TOR_INSTANCES: config.NUMBER_OF_TOR_INSTANCES,
-  START_TOR_PORT: config.START_TOR_PORT,
-  RESTART_PERIOD: config.RESTART_PERIOD,
-  INSTANCES_USED_SIMULTANEOUSLY: config.INSTANCES_USED_SIMULTANEOUSLY,
-  TOR_TIMEOUT: config.TOR_TIMEOUT
-};
-proxy.setupEnvironment(proxySettings);
-proxy.startService();
